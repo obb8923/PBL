56fda84c3d8e36e7d1acebbad5a70b7e7b6cbe09
KimYooBin1 <kyb1208tg@gmail.com>
2024-01-15 17:06:56 +0900

    first_commit

---
+++ PBL_AD_Manager/src/main/java/team2/PBL_AD_Manager/PblAdManagerApplication.java
@@ -0,0 +1,13 @@
+package team2.PBL_AD_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PblAdManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PblAdManagerApplication.class, args);
+	}
+
+}
---
+++ PBL_AD_Manager/src/test/java/team2/PBL_AD_Manager/PblAdManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package team2.PBL_AD_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PblAdManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
