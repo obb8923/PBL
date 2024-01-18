1d8838346b0ff478cf092682f818e09cd301bb95
JeongBin Park <obb8923@naver.com>
2024-01-18 10:35:34 +0900

    181035

--- PBL_AD_Manager/src/main/resources/static/script/script.js
+++ PBL_AD_Manager/src/main/resources/static/script/script.js
@@ -1,60 +1,39 @@
 function openPopupContainer(){
     document.getElementById('popupContainer').style.display = 'flex';
 }
-
 function closePopupContainer(){
     document.getElementById('popupContainer').style.display = 'none';
 
 }
-function clickADDButton() {
-    document.getElementById('ADDButton').
-    addEventListener('click', function () {
-        document.getElementById('popup').style.display = 'flex';
-        openPopupContainer();
-    });
-}
-function closePopup(buttonId,popupId) {
-    closePopupContainer();
-    console.log("ad");
-    document.getElementById(buttonId).
-    addEventListener('click', function () {
-        document.getElementById(popupId).style.display = 'none';
-    });
-    console.log("add");
-}
 
+function closePopup(popupId) {
+    closePopupContainer();
+    document.getElementById(popupId).style.display = 'none';
+}
 function confirmAdd() {
     var companyName = document.getElementById('addCompanyName').value;
     var ADName = document.getElementById('addADName').value;
-    var ADCondition = document.getElementById('addADCondition').value;
+    var AgeCondition = document.getElementById('addAgeCondition').value;
+    var genderCondition = document.getElementById('addGenderCondition').value;
     var slot = document.getElementById('addSlot').value;
     var cost = document.getElementById('addCost').value;
     var dayStart = document.getElementById('addDayStart').value;
     var dayEnd = document.getElementById('addDayEnd').value;
 
-    if (companyName === "" || ADName === "" || ADCondition==="" || slot===""||cost===""||dayStart===""||dayEnd==="") {
-        alert("값을 입력하지 않았습니다.");
-    }
-    else {
-        document.getElementById('confirmAdd').addEventListener('click', function () {
-            document.getElementById('popupContainer').style.display = 'none';
-        });
+    if (companyName === "" || ADName === "" || genderCondition === "" || AgeCondition === "" || slot === "" || cost === "" || dayStart === "" || dayEnd === "") {
+        alert("값을 모두 입력하지 않았습니다.");
+    } else {
+        closePopup('popupAdd');
     }
 }
-
-function clickModify(){
-}
-function fetchAdDetails(adId) {
-}
-
-function clickDetail(adsId) {
-    fetchAdDetails(adsId);
-    document.getElementById('popupDetail').style.display = 'flex';
+function openPopup(popupId) {
     openPopupContainer();
-
+    document.getElementById(popupId).style.display = 'flex';
 }
 
-function clickDelete() {
+function submitUI() {
 
 }
-
+function paginatiom(){
+    window.location.href = 'http://localhost:8080/hi/1';
+}
